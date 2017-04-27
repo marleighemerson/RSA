7ed105bbf290e0941eb1bc2f04c62e0ec5be58df
marleighwilliams <marleighwilliams@158.104.180.203>
2017-04-26 21:49:53 -0700

    UPDATE

--- src/rsa/_idea/RSA_idea.java
+++ src/rsa/_idea/RSA_idea.java
@@ -13,7 +13,11 @@ import java.math.BigInteger;
 import java.util.Random;
 
 /**
- *
+ * Elizabeth Reed
+ * Jamie Leary
+ * Marleigh Williams
+ * 
+ * BEST GROUP EVER
  * @author marleighwilliams
  */
 public class RSA_idea {
@@ -24,19 +28,23 @@ public class RSA_idea {
     
     private Random r; 
     
+    //RSA Algorithm!
     public RSA_idea()
     {
         r = new Random();
         
-        p = BigInteger.probablePrime(lengthofbit, r);
+        // generating two prime numbers p,q
+        p = BigInteger.probablePrime(lengthofbit, r); //probablePrime (BigInteger Method) gives a positive integer that is probably a prime with the specified bit length
         
-        q = BigInteger.probablePrime(lengthofbit, r);
+        q = BigInteger.probablePrime(lengthofbit, r); //gives positive probable prime with specified bit length
         
         e = BigInteger.probablePrime(lengthofbit/2, r);
         
+        //computing n p*q
         n = p.multiply(q);
         
-        z = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        //computing z which is ((p-1)*(q-1)
+        z = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE)); //ONE = 1
        
         while(z.gcd(e).compareTo(BigInteger.ONE)>0 && e.compareTo(z)<0)
         {
@@ -46,23 +54,13 @@ public class RSA_idea {
         d = e.modInverse(z);
         }
     
-    public RSA_idea(BigInteger e, BigInteger d, BigInteger n){
-        
-        this.d = d;
-        
-        this.n = n;
-        
-        this.e = e;
-        
-        
-    }
     
  
     public static void main(String[] args) throws IOException{
         
         RSA_idea rsa = new RSA_idea();
         
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(in)); //allows user input
         
         String checkString;
         
@@ -70,15 +68,15 @@ public class RSA_idea {
         
         checkString = br.readLine();
         
-        System.out.println("Encrypted String: " + checkString);
+        System.out.println("String to be Encrypted: " + checkString);
         
-        System.out.println("String in Bytes: " + byteMyString(checkString.getBytes()));
+        System.out.println("Encryption: " + byteMyString(checkString.getBytes()));
         
         byte[] encrypt = rsa.encrypt(checkString.getBytes());
         
         byte[] decrypt = rsa.decrypt(encrypt);
         
-        System.out.println("Decrypted Bytes: " + byteMyString(decrypt));
+        System.out.println("Decryption: " + byteMyString(decrypt));
         
         System.out.println("Decrypted String: " + new String(decrypt));
     }
